a478046c1d9e940709541e41f0440315cc319cf4
Zaman0070 <artfunjoin@gmail.com>
2022-02-10 13:26:01 +0500

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:feras_pay/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,8 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ,
+    return Sizer(
+        builder: (context, orientation, deviceType){
+          return  MaterialApp(
+            home: SplashScreen(),
+          );
+        }
+
     );
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,9 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-class SplashScreen extends StatelessWidget {
+
+import 'auth/registeration_screen.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+      const Duration(seconds: 3),
+          () => Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: ((context) => RegistrationScreen()))),
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Hero(
+          tag: 'logo',
+          child: SizedBox(
+            width: 250,
+            height: 250,
+            child: Image.asset('assets/images/Splash Page.png'),
+          ),
+        ),
+      ),
+    );
   }
 }
